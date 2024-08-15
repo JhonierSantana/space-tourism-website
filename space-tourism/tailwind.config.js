d59e1f7af7c9b52fdbfe42ec8094dd91a50a86e3
Jhonier Santana <jhonier_2504@hotmail.com>
2024-08-14 21:40:47 -0500

    Page/Destination

--- space-tourism/tailwind.config.js
+++ space-tourism/tailwind.config.js
@@ -6,27 +6,40 @@ export default {
       width: {
         "54p": "54%",
         "274px": "274px",
+        "445p": "445px",
       },
       height: {
         "382px": "382px",
         "274px": "274px",
+        "445p": "445px",
+        "472p": "472px",
+        "90vh": "90vh",
       },
       fontFamily: {
         "barlow-condensed": ["Barlow Condensed", "sans-serif"],
+        bellefair: ["Bellefair", "serif"],
+        barlow: ["Barlow", "sans-serif"],
       },
       spacing: {
-        '4.75px': '4.75px',
+        "4.75px": "4.75px",
       },
       fontSize: {
-        '28px': '28px',
+        "100p": "100px",
+        "28px": "28px",
+        "14px": "14px",
+      },
+      lineHeight: {
+        "114.6px": "114.6px",
       },
       letterSpacing: {
-        '4.75': '4.75px',
+        4.75: "4.75px",
       },
-
-      //maxWidth 
       maxWidth: {
-        '440px': '440px', 
+        "440px": "440px",
+        "28rem": "28rem",
+      },
+      textColor: {
+        gray: "#d4d4d4",
       },
     },
   },
