c594379f5e86b7f5381b07281bf63ba4fa303c13
Gusnecio <gustavojosegarciaperez86@gmail.com>
2024-08-14 22:10:42 -0500

    correccion errores

--- space-tourism/tailwind.config.js
+++ space-tourism/tailwind.config.js
@@ -17,38 +17,23 @@ export default {
       },
       fontFamily: {
         "barlow-condensed": ["Barlow Condensed", "sans-serif"],
-<<<<<<< HEAD
-        "bellefair": ["Bellefair", "serif"],  // Añadida la fuente Bellefair
-=======
         bellefair: ["Bellefair", "serif"],
         barlow: ["Barlow", "sans-serif"],
->>>>>>> e65eaa3de97ba10990cd87b2136aac605a27358d
       },
       spacing: {
         "4.75px": "4.75px",
       },
       fontSize: {
-<<<<<<< HEAD
-        '28px': '28px',
-        '56px': '56px',  // Añadido tamaño de fuente 56px
-=======
         "100p": "100px",
         "28px": "28px",
         "14px": "14px",
       },
       lineHeight: {
         "114.6px": "114.6px",
->>>>>>> e65eaa3de97ba10990cd87b2136aac605a27358d
       },
       letterSpacing: {
         4.75: "4.75px",
       },
-<<<<<<< HEAD
-      lineHeight: {
-        '64.18px': '64.18px',  // Añadido line-height de 64.18px
-      },
-=======
->>>>>>> e65eaa3de97ba10990cd87b2136aac605a27358d
       maxWidth: {
         "440px": "440px",
         "28rem": "28rem",
@@ -57,6 +42,6 @@ export default {
         gray: "#d4d4d4",
       },
     },
+    plugins: [],  
   },
-  plugins: [],
 };
