d464d2105cdef27629c6f434ba95fe3d16f8a0e5
Jhonier Santana <jhonier_2504@hotmail.com>
2024-08-16 09:02:45 -0500

    navbar/hover

--- space-tourism/src/components/NavBar.jsx
+++ space-tourism/src/components/NavBar.jsx
@@ -10,21 +10,21 @@ const Navbar = () => {
 
       <div className="flex border border-gray-600 border-solid h-0.5 w-2/5 ml-32 absolute z-10"></div>
 
-      <div className="flex bg-opacity-10 bg-white h-24 w-54p items-center backdrop-blur-md">
-        <ul className="flex font-barlow-condensed space-x-8 text-white tracking-widest w-max m-auto">
-          <li className="relative mr-10 ml-24">
+      <div className="flex bg-opacity-10 bg-white h-24 w-54p items-center backdrop-blur-md relative">
+        <ul className="flex font-barlow-condensed gap-10 text-white tracking-widest w-max m-auto ">
+          <li className="relative ml-24">
             <NavLink
               to="/"
               className={({ isActive }) =>
                 isActive
                   ? "text-gray-300 after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-white"
-                  : ""
+                  : "hover:text-gray-400 hover:underline"
               }
             >
               <span className="mr-1">00</span> HOME
             </NavLink>
           </li>
-          <li className="relative">
+          <li className="relative ml-10">
             <NavLink
               to="/destination"
               className={({ isActive }) =>
@@ -36,7 +36,7 @@ const Navbar = () => {
               <span className="mr-1">01</span> DESTINATION
             </NavLink>
           </li>
-          <li className="relative pl-10">
+          <li className="relative ml-10">
             <NavLink
               to="/crew"
               className={({ isActive }) =>
@@ -48,7 +48,7 @@ const Navbar = () => {
               <span className="mr-1">02</span> CREW
             </NavLink>
           </li>
-          <li className="relative pl-10 pr-56">
+          <li className="relative ml-10 mr-56">
             <NavLink
               to="/technology"
               className={({ isActive }) =>
